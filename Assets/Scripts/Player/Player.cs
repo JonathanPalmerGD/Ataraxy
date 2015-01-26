66c1f809db7c9c0077f7c8f706ea9715b2137777
unknown <vantharion@gmail.com>
2015-01-26 00:00:16 -0500

    Removed a debug command.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -348,12 +348,6 @@ public class Player : Entity
 
 	void GetInput()
 	{
-		if (Input.GetKeyDown(KeyCode.Period))
-		{
-			Debug.LogError(Shader.Find("Particles/Alpha Blended").ToString() + "\n");
-			Debug.LogError(new Material(Shader.Find("Particles/Alpha Blended")).ToString() + "\n");
-			//Debug.LogError(new Material(Shader.Find("Particles/Alpha Blended")).ToString() + "\n");
-		}
 		if (!UIManager.Instance.paused)
 		{
 			#region Mouse Buttons 1
