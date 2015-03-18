ef064ef7b40a2732c357cb32e8e52a4315ce1893
Jonathan Palmer <vantharion@gmail.com>
2015-03-18 15:11:17 -0400

    Menu Usability tweaks part 2

---
+++ Assets/Scripts/UI/ForceMouseUnlock.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceMouseUnlock : MonoBehaviour 
+{
+	void Update()
+	{
+		Screen.lockCursor = false;
+		Screen.showCursor = true;
+	}
+}
